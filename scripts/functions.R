2456a6fb73a2d86070e08aec1742029a6ba88737
Cattiva <alexander.nickel@mailbox.org>
2022-04-05 15:02:57 +0200

    commit again

--- scripts/functions.R
+++ scripts/functions.R
@@ -1,17 +1,24 @@
 
 
-
 connect_to_DB <- function(mydb, group = "fin_data"){
   
   Checkmydb <- tryCatch(DBI::dbIsValid(mydb),
                         error=function(e) e)
   if(inherits(Checkmydb, "simpleError")){
     
-    mydb <- DBI::dbConnect(RMariaDB::MariaDB(), group = group)
+    if(Sys.info()[[1]] == "Windows"){ #For testing on Windows
+      
+      mydb <- DBI::dbConnect(RMariaDB::MariaDB(), group = group, 
+                             default.file = stringr::str_c(here::here(),
+                                                           "/.my.cnf"))
+      
+    } else { #Production on Linux
+      
+      mydb <- DBI::dbConnect(RMariaDB::MariaDB(), group = group)
+      
+    }
   }
 }
-
-
 
 
 
